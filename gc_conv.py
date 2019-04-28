0183fb620ab97b551824f22d52397a1fbcf17b9c
Somshubra Majumdar <titu1994@gmail.com>
2019-04-28 13:52:21 -0500

    Initial commit

---
+++ gc_conv.py
@@ -0,0 +1,190 @@
+
+from keras.layers import Conv1D, Conv2D, Conv3D
+from keras.layers import Dense
+from keras.layers import MaxPool1D
+from keras.layers import Reshape
+from keras.layers import Activation
+from keras.layers import Softmax
+from keras.layers import GlobalAveragePooling2D
+from keras.layers import add, multiply, dot
+from keras.layers import Permute
+from keras.layers import Lambda
+
+from keras import backend as K
+
+from group_norm import GroupNormalization
+
+
+def global_context_block(ip, reduction_ratio=16, transform_activation='linear'):
+    """
+    Adds a Global Context attention block for self attention to the input tensor.
+    Input tensor can be or rank 3 (temporal), 4 (spatial) or 5 (spatio-temporal).
+
+    # Arguments:
+        ip: input tensor
+        intermediate_dim: The dimension of the intermediate representation. Can be
+            `None` or a positive integer greater than 0. If `None`, computes the
+            intermediate dimension as half of the input channel dimension.
+        reduction_ratio: Reduces the input filters by this factor for the
+            bottleneck block of the transform submodule. Node: the reduction
+            ratio must be set such that it divides the input number of channels,
+        transform_activation: activation function to apply to the output
+            of the transform block. Can be any string activation function availahle
+            to Keras.
+
+    # Returns:
+        a tensor of same shape as input
+    """
+    channel_dim = 1 if K.image_data_format() == 'channels_first' else -1
+    ip_shape = K.int_shape(ip)
+
+    # check rank and calculate the input shape
+    if len(ip_shape) == 3:  # temporal / time series data
+        rank = 3
+        batchsize, dim1, channels = ip_shape
+
+    elif len(ip_shape) == 4:  # spatial / image data
+        rank = 4
+
+        if channel_dim == 1:
+            batchsize, channels, dim1, dim2 = ip_shape
+        else:
+            batchsize, dim1, dim2, channels = ip_shape
+
+    elif len(ip_shape) == 5:  # spatio-temporal / Video or Voxel data
+        rank = 5
+
+        if channel_dim == 1:
+            batchsize, channels, dim1, dim2, dim3 = ip_shape
+        else:
+            batchsize, dim1, dim2, dim3, channels = ip_shape
+
+    else:
+        raise ValueError('Input dimension has to be either 3 (temporal), 4 (spatial) or 5 (spatio-temporal)')
+
+    if rank > 3:
+        flat_spatial_dim = -1 if K.image_data_format() == 'channels_first' else 1
+    else:
+        flat_spatial_dim = 1
+
+    """ Context Modelling Block """
+    # [B, ***, C] or [B, C, ***]
+    input_flat = _spatial_flattenND(ip, rank)
+    # [B, ..., C] or [B, C, ...]
+    context = _convND(ip, rank, channels=1, kernel=1)
+    # [B, ..., 1] or [B, 1, ...]
+    context = _spatial_flattenND(context, rank)
+    # [B, ***, 1] or [B, 1, ***]
+    context = Softmax(axis=flat_spatial_dim)(context)
+
+    # Compute context block outputs
+    context = dot([input_flat, context], axes=flat_spatial_dim)
+    # [B, C, 1]
+    context = _spatial_expandND(context, rank)
+    # [B, C, 1...] or [B, 1..., C]
+
+    """ Transform block """
+    # Transform bottleneck
+    # [B, C // R, 1...] or [B, 1..., C // R]
+    transform = _convND(context, rank, channels // reduction_ratio, kernel=1)
+    # Group normalization acts as Layer Normalization when groups = 1
+    transform = GroupNormalization(groups=1, axis=channel_dim)(transform)
+    transform = Activation('relu')(transform)
+
+    # Transform output block
+    # [B, C, 1...] or [B, 1..., C]
+    transform = _convND(transform, rank, channels, kernel=1)
+    transform = Activation(transform_activation)(transform)
+
+    # apply context transform
+    out = add([ip, transform])
+
+    return out
+
+
+def _convND(ip, rank, channels, kernel=1):
+    assert rank in [3, 4, 5], "Rank of input must be 3, 4 or 5"
+
+    if rank == 3:
+        x = Conv1D(channels, kernel, padding='same', use_bias=False, kernel_initializer='he_normal')(ip)
+    elif rank == 4:
+        x = Conv2D(channels, (kernel, kernel), padding='same', use_bias=False, kernel_initializer='he_normal')(ip)
+    else:
+        x = Conv3D(channels, (kernel, kernel, kernel), padding='same', use_bias=False, kernel_initializer='he_normal')(ip)
+
+    return x
+
+
+def _spatial_flattenND(ip, rank):
+    assert rank in [3, 4, 5], "Rank of input must be 3, 4 or 5"
+
+    ip_shape = K.int_shape(ip)
+    channel_dim = 1 if K.image_data_format() == 'channels_first' else -1
+
+    if rank == 3:
+        x = ip  # identity op for rank 3
+
+    elif rank == 4:
+        if channel_dim == 1:
+            # [C, D1, D2] -> [C, D1 * D2]
+            shape = [ip_shape[1], ip_shape[2] * ip_shape[3]]
+        else:
+            # [D1, D2, C] -> [D1 * D2, C]
+            shape = [ip_shape[1] * ip_shape[2], ip_shape[3]]
+
+        x = Reshape(shape)(ip)
+
+    else:
+        if channel_dim == 1:
+            # [C, D1, D2, D3] -> [C, D1 * D2 * D3]
+            shape = [ip_shape[1], ip_shape[2] * ip_shape[3] * ip_shape[4]]
+        else:
+            # [D1, D2, D3, C] -> [D1 * D2 * D3, C]
+            shape = [ip_shape[1] * ip_shape[2] * ip_shape[3], ip_shape[4]]
+
+        x = Reshape(shape)(ip)
+
+    return x
+
+
+def _spatial_expandND(ip, rank):
+    assert rank in [3, 4, 5], "Rank of input must be 3, 4 or 5"
+
+    channel_dim = 1 if K.image_data_format() == 'channels_first' else -1
+
+    if rank == 3:
+        x = ip  # identity op for rank 3
+
+    elif rank == 4:
+        if channel_dim == 1:
+            # [C, D1, D2] -> [C, D1 * D2]
+            shape = [-1, 1, 1]
+        else:
+            # [D1, D2, C] -> [D1 * D2, C]
+            shape = [1, 1, -1]
+
+        x = Reshape(shape)(ip)
+
+    else:
+        if channel_dim == 1:
+            # [C, D1, D2, D3] -> [C, D1 * D2 * D3]
+            shape = [-1, 1, 1, 1]
+        else:
+            # [D1, D2, D3, C] -> [D1 * D2 * D3, C]
+            shape = [1, 1, 1, -1]
+
+        x = Reshape(shape)(ip)
+
+    return x
+
+
+if __name__ == '__main__':
+    from keras.layers import Input
+    from keras.models import Model
+
+    ip = Input(shape=(64, 64, 32))
+    x = global_context_block(ip, reduction_ratio=16)
+
+    model = Model(ip, x)
+
+    model.summary()
